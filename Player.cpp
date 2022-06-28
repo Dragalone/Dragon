9ed1b40d74d4d7e4336faa5571a96952f922fd9d
Dragalone <vova.kaverya@mail.ru>
2022-06-28 14:01:13 +0300

    init

--- Player.cpp
+++ Player.cpp
@@ -24,20 +24,20 @@ void Player::handleInput()
 
     if(InputHandler::Instance()->isKeyDown(SDL_SCANCODE_D))
     {
-        m_velocity.setX(3);
+        m_velocity.setX(6);
     }
     if(InputHandler::Instance()->isKeyDown(SDL_SCANCODE_A))
     {
-        m_velocity.setX(-3);
+        m_velocity.setX(-6);
 
     }
     if(InputHandler::Instance()->isKeyDown(SDL_SCANCODE_W))
     {
-        m_velocity.setY(-3);
+        m_velocity.setY(-6);
     }
     if(InputHandler::Instance()->isKeyDown(SDL_SCANCODE_S))
     {
-        m_velocity.setY(3);
+        m_velocity.setY(6);
     }
 
     if(InputHandler::Instance()->isKeyUp(SDL_SCANCODE_D))
@@ -60,4 +60,12 @@ void Player::handleInput()
         if (m_velocity.getY()>0)
         m_velocity.setY(0);
     }
+    if (m_position.getY()<0)
+        m_position.setY(0);
+    if (m_position.getY()>930)
+        m_position.setY(930);
+    if (m_position.getX()<0)
+        m_position.setX(0);
+    if (m_position.getX()>1460)
+        m_position.setX(1460);
 }
